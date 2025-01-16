3e3646beb2fc50bbb66abb9d0929c6dcc716fe97
PAvel <95528324+Pavel-L-40@users.noreply.github.com>
2025-01-16 12:39:15 +0300

    валидация данных

--- m_16_2.py
+++ m_16_2.py
@@ -19,12 +19,12 @@ async def info_user(username: Annotated[str, Path(
     max_length= 20,
     description= 'Enter username',
     example= 'UrbanUser'
-)], age: int= Path(
+)], age: Annotated [int, Path(
     ge= 18,
     le= 120,
     description= 'Enter age',
     example= 24
-)):
+)]):
     return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
 
 
